e3d7fc1317f016a897ad9a4948ecd73570f9a70b
偶像是萧炎 <18188003473@qq.com>
2025-09-27 18:26:15 +0800

    main

---
+++ Demo.java
@@ -0,0 +1,18 @@
+public class Demo {
+    public static void main(String[] args){
+        String name = "黑马谢广坤";
+        int age = 18;
+        double height = 1.81;
+        char gender = '男';
+        boolean m = true;
+        System.out.println("哈哈哈，我是你爹");
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(gender);
+        System.out.println(m);
+        
+
+    }
+    
+}
